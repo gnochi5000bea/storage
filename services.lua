3f2eaac607d57948906a699f4488a6abe3ffb268
cunning <76720185+gnochi5000bea@users.noreply.github.com>
2025-08-23 19:18:30 -0700

    Delete services.lua

--- services.lua
+++
@@ -1,8 +0,0 @@
-local ServiceNames = {"UserInputService", "HttpService", "GuiService", "RunService", "Stats", "CoreGui", "TweenService", "Players", "Workspace"} 
-local Services = {} 
-
-for _,Service in ServiceNames do 
-    Services[Service] = cloneref(game:GetService(Service))
-end 
-
-return Services
